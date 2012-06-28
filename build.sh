1fc1504b77d69caca8ad408e10160a30e2d74a70
Nicolas Bonamy <nicolas@bonamy.fr>
2012-06-28 20:36:46 +0200

    Fix

--- build.sh
+++ build.sh
@@ -1,21 +1,17 @@
 
 # configuration
-ROOT=$HOME/Desktop
-TARGET='iPhoto Exporter.app'
+TARGET='launcher/build/Release/iPhoto Exporter.app'
 
-# delete previous
-rm -rf "$ROOT/$TARGET"
-
-# create application bundle
-mkdir -p "$ROOT/$TARGET/Contents/MacOS"
-mkdir -p "$ROOT/$TARGET/Contents/Resources"
+# build launcher
+cd launcher
+xcodebuild
+cd -
 
 # copy contents
-rsync -avC --exclude='.*' --exclude='data/*' --include='.htaccess' src/ "$ROOT/$TARGET/Contents/MacOS"
-rsync -avC --exclude='.*' bin/ "$ROOT/$TARGET/Contents/MacOS/bin"
-chmod +x "$ROOT/$TARGET/Contents/MacOS/bin"/*
+rsync -avC --exclude='.*' --exclude='data/*' --include='.htaccess' src/ "$TARGET/Contents/MacOS/src"
+rsync -avC --exclude='.*' bin/ "$TARGET/Contents/MacOS/bin"
+chmod +x "$TARGET/Contents/MacOS/bin"/*
 
-# move build files
-cp "build/Info.plist" "$ROOT/$TARGET/Contents/"
-cp "build/iphoto2mac.icns" "$ROOT/$TARGET/Contents/Resources/"
+# copy build files
+cp -R "$TARGET" .
 
