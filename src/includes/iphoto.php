2c79db1ce60ea729d4a2fb031f957987de8b1c86
nbonamy <nicolas@bonamy.fr>
2012-06-29 07:39:41 +0200

    fix for unknown media type

--- src/includes/iphoto.php
+++ src/includes/iphoto.php
@@ -1,24 +1,24 @@
 <?php
 
 class iPhoto {
-  
+
   private static $plist;
-  
-  
+
+
   private static function getPlist() {
-  
+
     // check cached version
     if (iPhoto::$plist == null) {
       $parser = new plistParser();
       iPhoto::$plist = $parser->parseFile(ALBUM_XML_LOCATION);
     }
-    
+
     // done
     return iPhoto::$plist;
   }
-  
+
   public static function getRolls() {
-  
+
     // get plist
     $plist = iPhoto::getPlist();
 
@@ -39,32 +39,31 @@ class iPhoto {
     // done
     return $rolls;
   }
-  
+
   public static function getImages() {
 
     // get plist
     $plist = iPhoto::getPlist();
-    
+
     // build images array
     $images = array();
     foreach ($plist['Master Image List'] as $id => $plist_image) {
-      
+
       // check
       if (in_array($plist_image['MediaType'], array('Image', 'Movie')) == false) {
-        return;
+        continue;
       }
-      
+
       // build image
       $image = Image::FromPlist($plist_image);
-      
+
       // done
       $images[$id] = $image;
     }
-    
-    
+
+
     // done
     return $images;
   }
-  
 
 }
